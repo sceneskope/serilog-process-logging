9dd2d472198906a816597568317bc2b8de5485b3
Nick Randell <nick@nickrandell.com>
2017-06-28 08:53:42 +0100

    Initial version

---
+++ serilog-process-logging/ProcessInformation.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Serilog.ProcessLogging
+{
+    public class ProcessInformation
+    {
+        public int GC0 { get; }
+        public int GC1 { get; }
+        public int GC2 { get; }
+        public int TotalMemory { get; }
+        public int NonpagedSystemMemorySize { get; }
+        public int PagedMemorySize { get; }
+        public int PagedSystemMemorySize { get; }
+        public int PeakPagedMemorySize { get; }
+        public int PeakVirtualMemorySize { get; }
+        public int PeakWorkingSet { get; }
+        public int PrivateMemorySize { get; }
+        public DateTime StartTime { get; }
+        public int ThreadCount { get; }
+        public TimeSpan TotalProcessorTime { get; }
+        public TimeSpan UserProcessorTime { get; }
+        public int VirtualMemorySize { get; }
+        public int WorkingSet { get; }
+
+        private static int ToMb(long value) => (int)(value / 1048576L);
+
+        public ProcessInformation(Process process)
+        {
+            GC0 = GC.CollectionCount(0);
+            GC1 = GC.CollectionCount(1);
+            GC2 = GC.CollectionCount(2);
+            TotalMemory = ToMb(GC.GetTotalMemory(false));
+            NonpagedSystemMemorySize = ToMb(process.NonpagedSystemMemorySize64);
+            PagedMemorySize = ToMb(process.PagedMemorySize64);
+            PagedSystemMemorySize = ToMb(process.PagedSystemMemorySize64);
+            PeakPagedMemorySize = ToMb(process.PeakPagedMemorySize64);
+            PeakVirtualMemorySize = ToMb(process.PeakVirtualMemorySize64);
+            PeakWorkingSet = ToMb(process.PeakWorkingSet64);
+            PrivateMemorySize = ToMb(process.PrivateMemorySize64);
+            StartTime = process.StartTime;
+            ThreadCount = process.Threads.Count;
+            TotalProcessorTime = process.TotalProcessorTime;
+            UserProcessorTime = process.UserProcessorTime;
+            VirtualMemorySize = ToMb(process.VirtualMemorySize64);
+            WorkingSet = ToMb(process.WorkingSet64);
+        }
+
+        public ProcessInformation(ProcessInformation previous, ProcessInformation current)
+        {
+            GC0 = current.GC0 - previous.GC0;
+            GC1 = current.GC1 - previous.GC1;
+            GC2 = current.GC2 - previous.GC2;
+            TotalMemory = current.TotalMemory - previous.TotalMemory;
+            NonpagedSystemMemorySize = current.NonpagedSystemMemorySize - previous.NonpagedSystemMemorySize;
+            PagedMemorySize = current.PagedMemorySize - previous.PagedMemorySize;
+            PagedSystemMemorySize = current.PagedSystemMemorySize - previous.PagedSystemMemorySize;
+            PeakPagedMemorySize = current.PeakPagedMemorySize - previous.PeakPagedMemorySize;
+            PeakVirtualMemorySize = current.PeakVirtualMemorySize - previous.PeakVirtualMemorySize;
+            PeakWorkingSet = current.PeakWorkingSet - previous.PeakWorkingSet;
+            PrivateMemorySize = current.PrivateMemorySize - previous.PrivateMemorySize;
+            StartTime = current.StartTime;
+            ThreadCount = current.ThreadCount - previous.ThreadCount;
+            TotalProcessorTime = current.TotalProcessorTime - previous.TotalProcessorTime;
+            UserProcessorTime = current.UserProcessorTime - previous.UserProcessorTime;
+            VirtualMemorySize = current.VirtualMemorySize - previous.VirtualMemorySize;
+            WorkingSet = current.WorkingSet - previous.WorkingSet;
+        }
+    }
+}
---
+++ serilog-process-logging/ProcessLogger.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using Serilog;
+
+namespace Serilog.ProcessLogging
+{
+    public class ProcessLogger
+    {
+        public static ProcessLogger StartNew(ILogger log, CancellationToken ct)
+        {
+            var logger = new ProcessLogger(log);
+            logger.RunAsync(ct);
+            return logger;
+        }
+
+        public ILogger Log { get; }
+        public TimeSpan Delay { get; set; } = TimeSpan.FromMinutes(1);
+
+        private readonly Process _process;
+        private Task _runner;
+
+        public ProcessLogger(ILogger logger)
+        {
+            _process = Process.GetCurrentProcess();
+            Log = logger.ForContext("Process", _process.Id);
+        }
+
+        public Task RunAsync(CancellationToken ct)
+        {
+            _runner = PeriodicDumperAsync(ct);
+            return _runner;
+        }
+
+        private async Task PeriodicDumperAsync(CancellationToken ct)
+        {
+            while (!ct.IsCancellationRequested)
+            {
+                try
+                {
+                    Dump();
+                }
+                catch (Exception ex) when (!(ex is OperationCanceledException))
+                {
+                    Log.Warning(ex, "Failed to dump: {exception}", ex.Message);
+                }
+                await Task.Delay(Delay, ct).ConfigureAwait(false);
+            }
+        }
+
+        private ProcessInformation _latestInformation;
+
+        private void Dump()
+        {
+            var information = new ProcessInformation(_process);
+            var logger = Log.ForContext("Information", information, true);
+
+            TimeSpan deltaProcessorTime;
+
+            if (_latestInformation != null)
+            {
+                var delta = new ProcessInformation(_latestInformation, information);
+                logger = logger.ForContext("Delta", delta, true);
+                deltaProcessorTime = delta.TotalProcessorTime;
+            }
+            else
+            {
+                deltaProcessorTime = information.TotalProcessorTime;
+            }
+
+            _latestInformation = information;
+            logger.Information("Process @ {ram} ({working}) used {processor} seconds", information.TotalMemory, information.WorkingSet, deltaProcessorTime.TotalSeconds);
+        }
+    }
+}
